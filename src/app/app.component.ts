be722537d2445387af0c5a21f7e367c9c1404731
HuberPascal <136008239+HuberPascal@users.noreply.github.com>
2025-01-11 13:25:56 +0100

    Implement navigation section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'myShop';
+
+  private router = inject(Router);
 }
---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  standalone: true,
+  imports: [],
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.scss',
+})
+export class NavComponent {
+  allNAvListItems: string[] = [
+    'IT + Multimedia',
+    'Haushalt',
+    'Baumarkt',
+    'Wohnen',
+    'Sport',
+    'Mode',
+    'Spielzeug',
+    'Baby + Eltern',
+    'Beauty + Gesundheit',
+    'Büro + Papeterie',
+    'Bücher',
+    'Supermarkt',
+  ];
+
+  allListItemstopTenCategory: string[] = [
+    'Windeln',
+    'Lego',
+    'Reinigungsmittel',
+    'Vitamine + Nahrungsergänzung',
+    'Gesellschaftsspiel',
+    'Reinigungsutensil',
+    'Sportnahrung',
+    'Zahnbürstenkopf',
+    'Waschmittel + Textilpflege',
+    'Trinkflasche Thermosflasche',
+  ];
+
+  alldirektToItems: string[] = [
+    'Produkte verkaufen',
+    'Galaxus Mobile',
+    'Gutscheine',
+    'Magazin',
+    'Form',
+  ];
+}
