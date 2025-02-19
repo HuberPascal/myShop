80ec46dedfc7333b563ce4cceb8495de623fd3e9
Pascal Huber <pascal.huber92@gmx.ch>
2025-02-19 16:32:53 +0100

    add cart routerLink to header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { RouterLink } from '@angular/router';
 
 @Component({
-    selector: 'app-header',
-    imports: [MatTooltipModule],
-    templateUrl: './header.component.html',
-    styleUrl: './header.component.scss'
+  selector: 'app-header',
+  imports: [MatTooltipModule, RouterLink],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {}
